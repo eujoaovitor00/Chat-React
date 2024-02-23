3f4eda58e9b0ec0558f0d4e386e25e5fdb114934
Jõao Vitor <vitorbraga2003@yahoo.com.br>
2024-02-23 16:59:42 -0300

    teste

---
+++ src/components/ChatMessages.tsx
@@ -0,0 +1,25 @@
+import { useChat } from "@/contexts/ChatContext";
+import { useUser } from "@/contexts/UserContext";
+
+export const ChatMessages = () => {
+    const chatCtx = useChat();
+    const UserCtx = useUser();
+    return(
+        <div className=" flex flex-col gap-1 ">
+            {chatCtx?.chat.map(item => (
+                <div
+                    key={item.id}
+                    className={`border border-white/20 rounded-md p-2 text-sm 
+                       ${item.user === UserCtx?.user ? 
+                        'self-end  bg-green-800 text-rigth ' :
+                        'self-start bg-white/5 text-left ' }
+                    `}
+                    >
+                    
+                        <div className=" max-w-full font-bold">{item.user}</div>
+                        <p>{item.text}</p>
+                </div>
+            ))}
+        </div>
+    )
+}
---
+++ src/reducers/ChatReducer.ts
@@ -0,0 +1,25 @@
+import { Message } from "@/types/Message";
+
+type AddAAction = {
+    type: 'add';
+    payload: {
+        user:string;
+        text:string;
+    }
+}
+
+type ChatActions = AddAAction;
+
+
+export const ChatReducer = (state: Message[], action: ChatActions) => {
+    switch(action.type) {
+        case 'add':
+        return [...state, {
+            id: state.length,
+            user: action.payload.user,
+            text: action.payload.text
+        }];
+        default:
+            return state;
+    }
+}
